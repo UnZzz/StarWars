398cdca1514f57ca720bd35273bce14c8917ecca
Cindy <cindyli28.mail@gmail.com>
2026-01-31 14:03:43 -0500

    hard code color -> var

--- enemy/tasks/grow_big.gd
+++ enemy/tasks/grow_big.gd
@@ -6,6 +6,7 @@ extends BTAction
 @export var growth_factor: float = 2.0
 @export var growth_duration: float = 5.0
 @export var max_scale: float = 5.0
+@export var color: Color = Color.RED
 
 var _has_grown: bool = false
 var _original_scale: float = 1.0
@@ -96,7 +97,7 @@ func _grow_enemy(enemy: BaseStar) -> void:
 	var original_mat = star_body.get_active_material(0)
 	if original_mat:
 		var glow = original_mat.duplicate() as StandardMaterial3D
-		glow.albedo_color = Color.RED
+		glow.albedo_color = color
 		star_body.set_surface_override_material(0, glow)
 
 func _shrink_back(enemy: BaseStar) -> void:
