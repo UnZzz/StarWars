23e9c1f5b85d195d7efe45582800a101e6528774
Cindy <cindyli28.mail@gmail.com>
2026-01-31 06:23:39 -0500

    happy -> scary face when close to player

--- enemy/base_star.gd
+++ enemy/base_star.gd
@@ -4,16 +4,36 @@ class_name BaseStar
 var type = 1
 
 @export var gravity : float = 0.98
+@export var scary_distance : float = 5.0
+
+@onready var scary_face : MeshInstance3D = $ScaryFace
+@onready var happy_face : MeshInstance3D = $HappyFace
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	scary_face.visible = false
+	happy_face.visible = true
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-	
+	update_face_based_on_distance()
+
+func update_face_based_on_distance() -> void:
+	if PlayerManager and PlayerManager.now_player:
+		var player = PlayerManager.now_player
+		var player_pos = player.global_position
+		var distance_to_player = global_position.distance_to(player_pos)
+		
+		if distance_to_player <= scary_distance:
+			scary_face.visible = true
+			happy_face.visible = false
+		else:
+			scary_face.visible = false
+			happy_face.visible = true
+	else:
+		scary_face.visible = false
+		happy_face.visible = true
+
 func _physics_process(delta: float) -> void:
 	velocity += Vector3.DOWN * gravity
 	move_and_slide()
