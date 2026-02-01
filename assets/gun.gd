8a0e22cb086d9f0441242ed6c935ae04ee22cfb2
aarynxie <150180442+aarynxie@users.noreply.github.com>
2026-02-01 12:30:52 -0500

    added particles to gun

--- assets/gun.gd
+++ assets/gun.gd
@@ -3,6 +3,11 @@ extends Node3D
 @onready var anim = $AnimationPlayer
 @onready var mask_gun = $".."
 
+
+@onready var particle1: GPUParticles3D = $particle/particlelargflash
+@onready var particle2: GPUParticles3D = $particle/particlecone
+@onready var particle3: GPUParticles3D = $particle/particleflash
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,9 +15,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	gun_animation()
+	if(Input.is_action_pressed("fire")):
+		gun_animation()
+		play_particles()
 
 func gun_animation():
-	if(Input.is_action_pressed("fire")):
-		anim.stop()
-		anim.play("HandAction")
+	anim.stop()
+	anim.play("HandAction")
+
+func play_particles():
+	particle1.restart()
+	particle1.emitting = true
+	
+	await get_tree().create_timer(0.2).timeout
+	particle1.emitting = false
