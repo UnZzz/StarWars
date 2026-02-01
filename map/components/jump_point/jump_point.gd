c137aae41ffe0a5e2413b203c7aba5583e556fb5
Un_Z <50923943+UnZzz@users.noreply.github.com>
2026-02-01 14:12:07 -0500

    Add cooldown to JumpPoint and update next map UID
    Introduces a cooldown mechanism for BaseStar objects using a static dictionary in JumpPoint, preventing immediate re-entry. Also updates the next_map UID in title_map.tscn and removes an unused texture import file.

--- map/components/jump_point/jump_point.gd
+++ map/components/jump_point/jump_point.gd
@@ -5,16 +5,29 @@ class_name JumpPoint
 
 var moving_bodies : Array[BaseStar]
 
+static var body_cd : Dictionary[BaseStar, float]
+
 
 func _physics_process(delta: float) -> void:
+	print(body_cd)
+	
+	for key in body_cd:
+		var value = body_cd[key]
+		body_cd[key] = value - delta
+	
 	for i in moving_bodies:
 		i.global_position = i.global_position.lerp(paired_jump_point.global_position ,0.1)
-		if i.global_position.distance_to(paired_jump_point.global_position):
+		var distance = i.global_position.distance_to(paired_jump_point.global_position)
+		if distance < 0.1:
 			moving_bodies.remove_at(moving_bodies.find(i))
 			i.process_mode = PROCESS_MODE_INHERIT
+			body_cd.set(i, 10.)
 			
 func _on_body_entered(body: Node3D) -> void:
-	if(body is BaseStar && !paired_jump_point.moving_bodies.has(body)):
+	#print("cd:",body_cd.get_or_add(body ,0.))
+	if(body is BaseStar && body_cd.get_or_add(body ,0.) <= 0.):
 		moving_bodies.append(body)
 		body.process_mode = PROCESS_MODE_DISABLED
+		body_cd[body as BaseStar] = 10.
+
 	pass # Replace with function body.
